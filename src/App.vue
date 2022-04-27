1b033b48357608ee8dee0a1be61493ab20fb7f00
dillin92 <dillin970@gmail.com>
2022-04-27 14:06:36 -0400

    setup vue project

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,16 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <HelloWorld msg="Welcome to Your Vue.js App"><img alt="Vue logo" src="./assets/logo.png"></HelloWorld>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Login from './components/Login.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    Login 
   }
 }
 </script>
