d5ab6af049940d4c9678f0d11eede3a320e40df8
dillin92 <dillin970@gmail.com>
2022-05-05 10:47:44 -0400

    stole a whole bunch of components from a FB clone

---
+++ src/app.config.js
@@ -0,0 +1,33 @@
+const firebaseConfig = {
+
+    apiKey: 'xxx-xxx-xxx-xxx-xxx-xxx-xxx-xxx',
+    
+    authDomain: 'xxx-xxx-xxx-xxx-xxx-xxx-xxx',
+    
+    databaseURL: 'xxx-xxx-xxx-xxx-xxx-xxx-xxx-xxx-xxx',
+    
+    projectId: 'xxx-xxx-xxx',
+    
+    storageBucket: 'xxx-xxx-xxx-xxx-xxx',
+    
+    messagingSenderId: 'xxx-xxx-xxx',
+    
+    appId: 'xxx-xxx-xxx-xxx-xxx-xxx-xxx-xxx',
+    
+    measurementId: 'xxx-xxx-xxx',
+    
+    },
+    
+    const cometChatConfig = {
+    
+    APP_ID: ' 208829aecf2511de',
+    
+    AUTH_KEY: '8e0a481daf7db03c099e5514eb9f86eae5a80da3',
+    
+    REST_KEY: '869d4473ce92ef7596093b4d9f89b53910c992f6',
+    
+    APP_REGION: 'us',
+    
+    }
+    
+    export { firebaseConfig, cometChatConfig }
